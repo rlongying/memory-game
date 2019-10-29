17bdf745f71073513047c65c5f7166965a349845
Ivan <wmj900823@gmail.com>
2019-10-29 15:44:08 -0700

    add terminate button and confirm modal

---
+++ public/js/constatns.js
@@ -0,0 +1,15 @@
+export const InitialValue = {
+  row: 5,
+  column: 5,
+  tiles: 4,
+  score: 0,
+  trial: 0
+};
+
+export const Audio = {
+  newGameSuccess: "new-game-success-sound",
+  flipSuccess: "flip-card-success-sound",
+  lastSuccess: "last-card-success-sound",
+  flipError: "flip-card-error-sound",
+  newGameFail: "new-game-failure-sound"
+};
--- public/js/game.js
+++ public/js/game.js
@@ -1,15 +1,46 @@
 import { getRndIntegers } from "./util.js";
+import { InitialValue, Audio } from "./constatns.js";
 
 // matrix dimension, min 5x5
-let matrixRow = 5;
-let matrixCol = 5;
+let matrixRow = InitialValue.row;
+let matrixCol = InitialValue.column;
 
 // number of tiles, initial value 5
-let tiles = 5;
+let tiles = InitialValue.tiles;
+let trial = InitialValue.trial;
+let score = InitialValue.score;
 
 // how many tiles have been clicked
 let clickCount = tiles;
 let hasError = false; // has chosen wrong cards
+
+const updateTiles = newTiles => {
+  tiles = newTiles;
+
+  let tilesSpan = document.getElementById("tiles");
+  tilesSpan.textContent = tiles;
+};
+
+const updateTrial = newTrial => {
+  trial = newTrial;
+
+  let trialSpan = document.getElementById("trial");
+  trialSpan.textContent = trial;
+};
+
+const updateScore = newScore => {
+  score = newScore;
+
+  let scoreSpan = document.getElementById("score");
+  scoreSpan.textContent = score;
+};
+
+const resetGameNumbers = () => {
+  // reset game info display
+  updateTrial(InitialValue.trial);
+  updateTiles(InitialValue.tiles);
+  updateScore(InitialValue.score);
+};
 
 const playSound = soundId => {
   let audio = document.getElementById(soundId);
@@ -22,21 +53,23 @@ let checkCard = event => {
   let hasSelected = card.dataset.selected;
 
   if (hasSelected === "true") {
+    updateScore(score + 1);
     // click correct card
     if (!hasError && clickCount === 1) {
       // correct card and is the last card
       card.classList.add("success-card");
-      playSound("last-card-success-sound");
+      playSound(Audio.lastSuccess);
     } else {
       // not last card
       card.classList.add("flip-card-single");
-      playSound("flip-card-success-sound");
+      playSound(Audio.flipSuccess);
     }
   } else {
+    updateScore(score - 1);
     // clicked wrong card
     hasError = true;
     card.classList.add("error-card");
-    playSound("flip-card-error-sound");
+    playSound(Audio.flipError);
   }
 
   clickCount--;
@@ -46,7 +79,7 @@ let checkCard = event => {
     flipMissedCards();
     setTimeout(() => {
       if (!hasError) {
-        playSound("new-game-success-sound");
+        playSound(Audio.newGameSuccess);
       }
       startNewRound();
     }, 2000);
@@ -125,11 +158,28 @@ const rotateMatrix = () => {
   container.classList.add("rotate-matrix");
 };
 
-const startNewRound = () => {
-  // update rows, and columns, tiles, clickCount, hasError
+const terminateGame = () => {
+  if (confirm("Are you sure to terminate the game?")) {
+    // yes
+    console.log("confirm termination");
+    resetGameNumbers();
+  }
+  // cancel do nothing
+};
 
+const setNewMatrix = () => {
+  if (hasError) {
+  }
   hasError = false;
   clickCount = tiles;
+};
+
+const startNewRound = () => {
+  // update rows, and columns, tiles, clickCount, hasError
+  setNewMatrix();
+
+  // display game infos
+  resetGameNumbers();
   // create new matrix
   createMatrix(matrixRow, matrixCol);
 
@@ -139,6 +189,10 @@ const startNewRound = () => {
   // rotate matrix
   rotateMatrix();
 };
+
+document
+  .getElementById("terminate-btn")
+  .addEventListener("click", terminateGame);
 
 window.onload = () => {
   startNewRound();
